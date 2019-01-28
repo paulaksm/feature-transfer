c13eab8a13f49309ecd2f13a308e14aab4b9b470
paulaksm <paulaksm@ime.usp.br>
2019-01-27 22:50:30 -0200

    Add checkpoints and training history

--- transfer/mobilenet-car-data.py
+++ transfer/mobilenet-car-data.py
@@ -1,24 +1,27 @@
+import os
+import PIL
+import json
+import shutil
+import datetime
+import numpy as np
 from keras import applications as pretrained
 from keras.models import Model
 from keras.preprocessing import image
 from keras import optimizers
-import numpy as np
-import PIL
 from progress.bar import Bar
 from keras.utils import to_categorical
 from keras.callbacks import EarlyStopping
 from keras.utils import np_utils
 from DataGenerator import DataGenerator
 
+dir_checkpoints = 'checkpoints'
+if os.path.exists(dir_checkpoints):
+    shutil.rmtree(dir_checkpoints)
+os.makedirs(dir_checkpoints)
 
-# X_train = np.load('/var/tmp/pksm/self_driving_data/data/train_data.npy')
+path_checkpoints = "weights-improvement-{epoch:02d}-{val_acc:.2f}.hdf5"
 
-# train = np.load('/var/tmp/pksm/self_driving_data/data/train_data.npy')
-
-# y_train = np.load('/var/tmp/pksm/self_driving_data/data/train_labels.npy')
-
-# # X_valid = np.load('/var/tmp/pksm/self_driving_data/data/valid_data.npy')
-# y_valid = np.load('/var/tmp/pksm/self_driving_data/data/valid_labels.npy')
+start_time = datetime.datetime.now()
 
 data_gen = DataGenerator('/var/tmp/pksm/self_driving_data/data/')
 X_train, y_train = data_gen.load_data(usage='train')
@@ -28,11 +31,8 @@ X_valid, y_valid = data_gen.load_data(usage='valid')
 X_valid, y_valid = data_gen.preprocess_data(X_valid, y_valid, balance='undersampling')
 
 
-
-##################
-
 batch_size = 256
-epochs = 5
+epochs = 15
 
 model = pretrained.mobilenet.MobileNet(weights=None, classes=3)
 
@@ -41,22 +41,23 @@ model.compile(loss='categorical_crossentropy',
               optimizer=sgd,
               metrics=['accuracy'])
 
-# y_train_hot = np_utils.to_categorical(y_train, 3)
-# y_valid_hot = np_utils.to_categorical(y_valid, 3)
-
-# X_train = X_train.reshape((-1, 224, 224, 3))
-# X_valid = X_valid.reshape((-1, 224, 224, 3))
 
 stopper = EarlyStopping(monitor='val_acc', min_delta=0.0001, patience=3, verbose=1)
 
-model.fit(X_train, 
-          y_train_hot, 
-          validation_data=(X_valid, y_valid), 
-          epochs=epochs, 
-          batch_size=batch_size,
-          callbacks = [stopper],
-          shuffle=True,
-          verbose=1)
+checkpoint = ModelCheckpoint(path_checkpoints, 
+                             monitor='val_acc', 
+                             verbose=1, 
+                             save_best_only=True, 
+                             mode='max')
+
+history = model.fit(X_train, 
+                    y_train, 
+                    validation_data=(X_valid, y_valid), 
+                    epochs=epochs, 
+                    batch_size=batch_size,
+                    callbacks = [stopper, checkpoint],
+                    shuffle=True,
+                    verbose=1)
 
 # model.fit_generator(generator(X_train, y_train, batch_size),
 #                     validation_data=generator(X_valid, y_valid, batch_size),
@@ -74,6 +75,15 @@ model.fit(X_train,
 
 model.save("self-driving-data-mobilenet.h5")
 print("Saved model to disk")
+
+file_name = 'hist_{}{}{}_{}{}.json'.format(start_time.year, 
+                                           start_time.month, 
+                                           start_time.day, 
+                                           start_time.hour, 
+                                           start_time.minute)
+with open(file_name, 'w') as file:
+  json.dump(history, file)
+
 score = model.evaluate(X_valid, y_valid, verbose=0)
 print('Valid loss:', score[0])
 print('Valid accuracy:', score[1])
