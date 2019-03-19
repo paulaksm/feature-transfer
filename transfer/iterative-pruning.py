24c9f9bd1a62daa9b35c2d5e4a7db329e09b11aa
paulaksm <paulaksm@ime.usp.br>
2019-03-19 15:14:23 -0300

    Fix typo

--- transfer/iterative-pruning.py
+++ transfer/iterative-pruning.py
@@ -9,10 +9,7 @@ from keras.models import Model
 from keras import optimizers
 from keras.callbacks import EarlyStopping, ModelCheckpoint, Callback
 from DataGenerator import DataGenerator
-from keras import applications as pretrained
-from keras.layers import GlobalAveragePooling2D, Conv2D, Activation, Reshape, Dropout
-from keras.backend import int_shape
-from keras.initializers import VarianceScaling
+from keras.layers import Dense, Flatten
 
 
 class CustomCallback(Callback):
@@ -33,8 +30,6 @@ def parse():
     return parser.parse_args()
 
 
-
-
 def train(dataset_path):
     path_checkpoints = '/content/gdrive/Team Drives/Models/model-improvement-{epoch:02d}-{val_acc:.2f}.hdf5'
     file_train = os.path.join(dataset_path, 'train_labels.npy')
@@ -51,42 +46,47 @@ def train(dataset_path):
     epochs = 25
 
     # Model
-    inputs = Input(shape=(45,80,3))
+    inputs = Input(shape=(45, 80, 3))
 
     x = Dense(276, activation='relu')(inputs)
     x = Flatten()(x)
     predictions = Dense(3, activation='softmax')(x)
 
     model = Model(inputs=inputs, outputs=predictions)
-    
+
     sgd = optimizers.SGD(lr=0.01, decay=0.0005, momentum=0.9)
     model.compile(loss='categorical_crossentropy',
                   optimizer=sgd,
                   metrics=['accuracy'])
 
-    stopper = EarlyStopping(monitor='val_acc', min_delta=0.0001, patience=3, verbose=1)
+    stopper = EarlyStopping(
+        monitor='val_acc', min_delta=0.0001, patience=3, verbose=1)
 
-    checkpoint = ModelCheckpoint(path_checkpoints, 
-                                 monitor='val_acc', 
-                                 verbose=1, 
-                                 save_best_only=True, 
+    checkpoint = ModelCheckpoint(path_checkpoints,
+                                 monitor='val_acc',
+                                 verbose=1,
+                                 save_best_only=True,
                                  mode='max')
-    
+
     custom_callback = CustomCallback()
 
     data_gen = DataGenerator(dataset_path)
 
     model.fit_generator(data_gen.npy_generator(usage='train', batch_size=batch_size),
-                        steps_per_epoch=np.ceil(x_train_samples / batch_size).astype(int),
-                        validation_data=data_gen.npy_generator(usage='valid', batch_size=batch_size),
-                        validation_steps=np.ceil(x_valid_samples / batch_size).astype(int),
+                        steps_per_epoch=np.ceil(
+                            x_train_samples / batch_size).astype(int),
+                        validation_data=data_gen.npy_generator(
+                            usage='valid', batch_size=batch_size),
+                        validation_steps=np.ceil(
+                            x_valid_samples / batch_size).astype(int),
                         callbacks=[stopper, checkpoint, custom_callback],
                         epochs=epochs,
                         verbose=1)
 
+
 def main():
     args = parse()
-    train(args.data, args.n_freeze)
+    train(args.data)
 
 
 if __name__ == '__main__':
