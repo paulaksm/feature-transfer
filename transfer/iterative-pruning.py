384cf5ea10d7576ffd9f4ccbc8158c37568f3146
paulaksm <paulaksm@ime.usp.br>
2019-03-21 15:55:36 -0300

    Add changes to iterative pruning svcript

--- transfer/iterative-pruning.py
+++ transfer/iterative-pruning.py
@@ -12,6 +12,15 @@ from keras.layers import Dense, Flatten, Input
 
 
 class CustomCallback(Callback):
+
+    def on_epoch_end(self, epoch, logs=None):
+        # zero out weights accordingly to prune_mask list 
+        final = []
+        iter_weights = self.model.get_weights()
+        for i in range(len(iter_weights)):
+            final.append(np.multiply(iter_weights[i], prune_mask[i]))
+        self.model.set_weights(final)
+
     def on_train_end(self, epoch, logs=None):
         test_data, test_labels = data_gen.load_data(usage='test')
         test_data, test_labels = data_gen.preprocess_data(test_data, 
@@ -27,7 +36,7 @@ class CustomCallback(Callback):
 
 
 def parse():
-    description = 'DFN self-driving car'
+    description = 'Iterative pruning - v0'
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('-data_path',
                         '--data',
@@ -39,13 +48,39 @@ def parse():
                         '--prune',
                         type=float, default=0.3,
                         help='percentage to prune per iteration (default=0.3)')
-    
+    parser.add_argument('-iter',
+                        '--iter',
+                        type=int, default=5,
+                        help='number of iterations (default=5)')
 
     return parser.parse_args()
 
 
-def train(dataset_path):
-    path_checkpoints = '/content/gdrive/Team Drives/Models/model-improvement-{epoch:02d}-{val_acc:.2f}.hdf5'
+
+def iterative(path_data, model, prune_ratio, iterations):
+    pr = 1
+    for i in range(iterations):
+        pr = pr * prune_ratio
+        all_weights = model.get_weights()
+        list_flat = []
+        for j in range(all_weights.shape[0]):
+          list_flat.append(all_weights[j].reshape(-1))
+        flat = np.concatenate(list_flat)
+        flat = sorted(map(abs, flat))
+        threshold = flat[int(len(flat) * (1-pr))]
+        del list_flat
+        global prune_mask
+        prune_mask = []
+        curr_weights = []
+        for i in range(len(all_weights)):
+            prune_mask.append(abs(all_weights[i]) > threshold)
+            curr_weights.append(np.multiply(all_weights[i], prune_mask[i]))
+        model = train(path_data, curr_weights)
+
+
+
+def train(dataset_path, custom_weights):
+    path_checkpoints = '/content/gdrive/Team Drives/Models/best_model.hdf5'
     file_train = os.path.join(dataset_path, 'train_labels.npy')
     file_valid = os.path.join(dataset_path, 'valid_labels.npy')
     x_train = np.load(file_train, mmap_mode='r')
@@ -62,12 +97,16 @@ def train(dataset_path):
     # Model
     inputs = Input(shape=(10800,)) #working with already flatten image
 
-    x = Dense(276, activation='relu')(inputs)
+    x = Dense(1333, activation='relu')(inputs)
+    x = Dense(200, activation='relu')(x)
     predictions = Dense(3, activation='softmax')(x)
 
     model = Model(inputs=inputs, outputs=predictions)
 
+    model.set_weights(custom_weights) # initializing weights from first training + pruned connections
+
     sgd = optimizers.SGD(lr=0.01, decay=0.0005, momentum=0.9)
+
     model.compile(loss='categorical_crossentropy',
                   optimizer=sgd,
                   metrics=['accuracy'])
@@ -85,7 +124,6 @@ def train(dataset_path):
     global data_gen
     data_gen = DataGenerator(dataset_path)
 
-
     model.fit_generator(data_gen.npy_generator(usage='train', batch_size=batch_size),
                         steps_per_epoch=np.ceil(
                             x_train_samples / batch_size).astype(int),
@@ -96,11 +134,12 @@ def train(dataset_path):
                         callbacks=[stopper, checkpoint, custom_callback],
                         epochs=epochs,
                         verbose=1)
+    return model
 
 
 def main():
     args = parse()
-    train(args.data)
+    iterative(args.data, args.model, args.prune, args.iter)
 
 
 if __name__ == '__main__':
